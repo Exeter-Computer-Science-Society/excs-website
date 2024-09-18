a55f384f17d774808cd4369b1550329c9690c268
inspizzz <wiktor.wiejak@gmail.com>
2024-09-17 20:10:16 -0600

    Removed the spacing around the top bar for it to match the page x's padding.

--- components/TopBar/index.jsx
+++ components/TopBar/index.jsx
@@ -6,7 +6,7 @@ import { FiExternalLink } from "react-icons/fi";
 
 export function TopBar() {
 	return (
-		<div className="w-full h-28 pl-[var(--page-padding-left)] pr-[var(--page-padding-right)] pt-6 flex gap-16 items-center justify-around fixed top-0 left-0 right-0 z-50 bg-background pb-6 border-b border-b-secondary">
+		<div className="w-full h-28 pl-[var(--page-padding-left)] pr-[var(--page-padding-right)] pt-6 flex gap-16 items-center justify-between fixed top-0 left-0 right-0 z-50 bg-background pb-6 border-b border-b-secondary">
 			<Link href={"/"}>
 				<Image src="/images/excs-final.svg" alt="Exeter Computer Science Society" width={150} height={150} />
 			</Link>
