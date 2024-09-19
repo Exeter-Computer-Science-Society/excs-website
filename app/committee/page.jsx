11e7a48961087ee3031fcaee29703c444ce22de7
inspizzz <wiktor.wiejak@gmail.com>
2024-09-19 11:22:08 -0600

    Testing.

--- app/committee/page.jsx
+++ app/committee/page.jsx
@@ -8,7 +8,7 @@ export default function CommitteePage() {
 	return (
 		<div className="w-full h-full pt-56 pl-[var(--page-padding-left)] pr-[var(--page-padding-right)]">
 			<h1 className="text-light_text text-4xl font-bold flex gap-1"><IoMdArrowDropright className="text-accent"/>Committee 2024/2025</h1>
-			<Committee data={new_committee} detailed={true} />
+			<Committee data={new_committee} detailed={false} />
 
 			<h1 className="text-light_text text-4xl font-bold flex gap-1 pt-12"><IoMdArrowDropright className="text-accent"/>Committee 2023/2024</h1>
 			<Committee data={old_committee} detailed={false} />
